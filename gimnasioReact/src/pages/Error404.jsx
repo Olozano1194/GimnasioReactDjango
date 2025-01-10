fb53c2a2dc0d7f9d9bf69fb2ca28b6c309535879
Oscar Lozano <olozano1194@gmail.com>
2025-01-10 02:19:24 -0500

    Se le agregaron estilos al componente de error

--- gimnasioReact/src/pages/Error404.jsx
+++ gimnasioReact/src/pages/Error404.jsx
@@ -21,7 +21,7 @@ const Error404 = () => {
     };
     return (
         <main className="w-full min-h-screen flex flex-col justify-center items-center">
-            <div className="text-center text-gray-900 font-bold">
+            <div className="text-center text-gray-900 font-bold items-center flex flex-col">
                 <span className='w-full flex justify-center items-center gap-4 text-4xl md:w-96'><FaExclamationTriangle className="text-yellow-400" />Error 404</span>
                 <h1 className='text-xl font-bold pt-4 pb-4 md:text-2xl xl:text-4xl'>¡Oops! Página no encontrada</h1>
                 <p className='text-md font-semibold px-3 pb-3 md:text-lg xl:text-2xl'>
