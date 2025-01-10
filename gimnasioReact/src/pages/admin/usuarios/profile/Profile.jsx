f68b6023db6da1368664b0c472b0617b06bb707a
Oscar Lozano <olozano1194@gmail.com>
2025-01-10 03:17:35 -0500

    Se modifico el componente profile para que mostrara los datos del usuario logueado y se le hizo sus respectivos cambios de estilos a ambos componentes

--- gimnasioReact/src/pages/admin/usuarios/profile/Profile.jsx
+++ gimnasioReact/src/pages/admin/usuarios/profile/Profile.jsx
@@ -10,7 +10,15 @@ import {useForm} from 'react-hook-form';
 const Profile = () => {
     
     const [user, setUser] = useState([]);
+    const { editingUser, seteditingUser } = useState(false);
+    const [loading, setLoading] = useState(true);
     const { handleSubmit, setValue } = useForm();
+    const [formData, setFormData] = useState({
+        name: '',
+        lastname: '',
+        roles: '',
+        avatar: '',
+    });
 
     //hook para vizualizar los datos de la bd
     useEffect(() => {
@@ -19,10 +27,14 @@ const Profile = () => {
                 const userData = await getUserProfile();
                 if (userData.user) {
                     setUser(userData.user);
-                    console.log('Datos de usuario:', userData.user);
-                    setValue('name', userData.user.name);
-                    setValue('lastName', userData.user.lastName);
-                    setValue('roles', userData.user.roles);
+                    //console.log('Datos de usuario:', userData.user);
+                    setFormData({
+                        name: userData.user.name,
+                        lastname: userData.user.lastname,
+                        roles: userData.user.roles,
+                        //avatar: userData.user.avatar,
+                    });
+                    setLoading(false);
                                 
                 }else {
                     console.log('Datos de usuario o estudiante faltantes:', userData);
@@ -30,16 +42,40 @@ const Profile = () => {
                 
             }catch (error) {
                 console.error('Error fetching data:', error);
+                setLoading(false);
             }
         };
         fetchData();
-    }, [setValue]);
+    }, []);
+
+    //Función donde manejamos los cambios en el formulario
+    const handleChanges = (data) => {
+        const { name, value } = data.target;
+        setFormData((prevData) => ({ 
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    //Función para guardar los cambios en el formulario
+    const handleSave = async () => {
+        try {
+            const updateUser = await updateProfile(formData);
+            if (updateUser) {
+                setUser(false);
+                console.log('Perfil actualizado: ', updateUser);
+                
+            }
+        }catch (err) {
+            console.error('Error al actualizar perfil:', err);
+        }
+    };
 
     return (
         <section className="bg-secondary p-8 rounded-xl">
-            <h1 className="text-xl text-gray-100">Profile</h1>
+            <h1 className="text-xl text-dark font-bold md:text-2xl">Profile</h1>
             <hr className="my-8 border-gray-500" />
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form>
                 {/* Avatar */}
                 <div className="flex items-center">
                     <div className="w-1/4">
@@ -47,8 +83,8 @@ const Profile = () => {
                     </div>                    
                     <div className="flex-1">
                         <div className="relative mb-2">
-                            <img src={'https://img.freepik.com/foto-gratis/negocios-finanzas-empleo-concepto-mujeres-emprendedoras-exitosas-joven-empresaria-segura-anteojos-mostrando-gesto-pulga-arriba-sostenga-computadora-portatil-garantice-mejor-calidad-servicio_1258-59118.jpg'} alt="Avatar" className="w-28 h-28 object-cover rounded-lg" />
-                            <label htmlFor="avatar" className="absolute bg-slate-700 p-2 rounded-full hover:cursor-pointer -top-2 left-24"><RiEdit2Line /></label>
+                            <img src={formData.avatar || 'https://img.freepik.com/foto-gratis/negocios-finanzas-empleo-concepto-mujeres-emprendedoras-exitosas-joven-empresaria-segura-anteojos-mostrando-gesto-pulga-arriba-sostenga-computadora-portatil-garantice-mejor-calidad-servicio_1258-59118.jpg'} alt="Avatar" className="w-28 h-28 object-cover rounded-lg" />
+                            <label htmlFor="avatar" className="absolute bg-secondary p-2 rounded-full hover:cursor-pointer -top-2 left-24"><RiEdit2Line className='text-primary' /></label>
                             <input type="file" name="avatar" id="avatar" className="hidden" 
                                 // {...register("avatar")} 
                                 accept='image/*' 
@@ -67,20 +103,26 @@ const Profile = () => {
                         <div className="w-full">
                             <input 
                                 type="text"
-                                className="w-full py-2 px-4 outline-none rounded-lg bg-slate-900"
+                                className="w-full py-2 px-4 outline-none rounded-lg bg-dark text-white"
                                 placeholder='Nombre(s)' 
                                 id="name"
                                 name='name'
+                                value={formData.name}
+                                onChange={handleChanges}
+                                disabled={!editingUser}
                             />                           
                         </div>
                         {/* Apellido */}
                         <div className="w-full">
                             <input 
                                 type="text" 
-                                className="w-full py-2 px-4 outline-none rounded-lg bg-slate-900"
+                                className="w-full py-2 px-4 outline-none rounded-lg bg-dark text-white"
                                 placeholder='Apellido(s)'
                                 id="lastName"
-                                name='lastName'                                   
+                                name='lastName'
+                                value={formData.lastname}
+                                onChange={handleChanges}
+                                disabled={!editingUser}                                   
                             />                                   
                         </div>
                     </div>                                      
@@ -94,8 +136,11 @@ const Profile = () => {
                         <div className="w-full md:w-1/2">
                             <select 
                                 name="roles" 
-                                id="" 
-                                className="w-full py-2 px-4 outline-none rounded-lg bg-slate-900 appearance-none text-slate-500"                                   
+                                id=""
+                                value={formData.roles}
+                                onChange={handleChanges}
+                                disabled={!editingUser} 
+                                className="w-full py-2 px-4 outline-none rounded-lg bg-dark text-white appearance-none"                                   
                             >
                                 <option value="">Selecione el Rol</option>
                                 <option value='admin'>Administrador</option>
@@ -103,7 +148,27 @@ const Profile = () => {
                             </select>                                   
                         </div>                                                
                     </div>
-                </div>                                          
+                </div>
+                {/* Botones de acción */}
+                <div className='flex justify-end items-center'>
+                    {editingUser ? (
+                    <button
+                        type="button"
+                        className="mt-4 py-2 px-4 bg-blue-500 text-white rounded"
+                        onClick={handleSave}
+                    >
+                        Guardar Cambios
+                    </button>
+                    ) : (
+                    <button
+                        type="button"
+                        className="mt-4 py-2 px-4 bg-orange-500 text-white rounded"
+                        onClick={() => seteditingUser(true)}
+                    >
+                        Editar Perfil
+                    </button>
+                    )}          
+                </div>                                
             </form>
         </section>
     );
