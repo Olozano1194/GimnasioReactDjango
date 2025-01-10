fce01399a81361eed10ae5bfb5f093602a5e3d2e
Oscar Lozano <olozano1194@gmail.com>
2025-01-10 01:56:27 -0500

    Se le estan haciendo modificaciones a los archivos donde se realizan las peticiones para ver si funcionan de ambas maneran en local y en producción

--- gimnasioReact/src/api/userGym.api.js
+++ gimnasioReact/src/api/userGym.api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const gymApi = axios.create({
-    baseURL: 'http://localhost:8000/gym/api/v1/',
-    //baseURL: 'https://gimnasioreactdjango.onrender.com/gym/api/v1/',
+    //gitbaseURL: 'http://localhost:8000/gym/api/v1/',
+    baseURL: import.meta.env.MODE === 'development' 
+        ? 'http://localhost:8000/gym/api/v1'
+        : 'https://gimnasioreactdjango.onrender.com/gym/api/v1',
     headers: {
         'Content-Type': 'application/json',
       },
--- gimnasioReact/src/api/users.api.js
+++ gimnasioReact/src/api/users.api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const gymApi = axios.create({
     //baseURL: 'http://localhost:8000/gym/api/v1/',
     baseURL: import.meta.env.MODE === 'development' 
-        ? import.meta.env.VITE_API_URL_DEV 
-        : import.meta.env.VITE_API_URL_PROD,
+    ? 'http://localhost:8000/gym/api/v1'
+    : 'https://gimnasioreactdjango.onrender.com/gym/api/v1',
     headers: {
         'Content-Type': 'application/json',
       },
