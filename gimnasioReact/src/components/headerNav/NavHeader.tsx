c19461d455ebfdb1e23bfb19dc254c74b3b82286
Oscar Lozano <srempalador2021@gmail.com>
2026-01-09 18:57:18 -0500

    refactor(auth): centraliza logout globalmente usando Auth y Context

--- gimnasioReact/src/components/headerNav/NavHeader.tsx
+++ gimnasioReact/src/components/headerNav/NavHeader.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
 // icons
 import { RiArrowDownSLine, RiSettings3Line, RiLogoutCircleRLine } from "react-icons/ri";
 //Enlaces
@@ -15,7 +16,7 @@ import NotificationMenu from "../../components/headerNav/NotificationMenu"
 function NavHeader() {
     const navigate = useNavigate();
     const [user, setUser] = useState({ name: '', lastName: '', email: '', avatar: ''});
-    const [, setLoggedOut] = useState<boolean>(false);    
+    const auth = useContext(AuthContext);    
 
     useEffect(() => {
         const axiosUserData = async () => {
@@ -42,73 +43,20 @@ function NavHeader() {
         axiosUserData();
     }, []);
 
+    if (!auth) return null;
+    
+    const { logout } = auth;
+
     // Esta función nos sirve para cerrar la sesión    
-    const handleLogOut = () => {
-        localStorage.removeItem('token');
-        setLoggedOut(true);
-        console.log('Token removed:', localStorage.getItem('token'));
+     const handleLogOut = () => {
+        logout();
         navigate('/');        
     }
     
     return (
         <nav className="flex items-center gap-x-2">
             {/* este seria el menu de las notificaciones */}
-            <Menu>
-                {/* <MenuButton className="relative hover:bg-slate-700 p-2 rounded-lg transition-colors">
-                    <RiNotification3Line className="text-2xl" />
-                    <span className="absolute -top-0.5 right-0 bg-sky-500 py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold">2</span>
-                </MenuButton> 
-                <MenuItems anchor='bottom end' className='bg-secondary mt-1 p-4 rounded-lg'>
-                    <h1 className="text-dark text-center font-medium">Notificaciones</h1>
-                    <hr className="my-6 border-gray-500" />
-                    <MenuItem as='div' className='p-0 hover:bg-slate-200'>
-                            <Link to="/notifications" 
-                                className="flex flex-1 items-center gap-x-2 py-2 px-4 hover:bg-slate-200 transition-colors rounded-lg text-dark">
-                                <img src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg" alt="" className="w-8 h-8 object-cover rounded-full" />
-                                <div className="text-sm flex flex-col">
-                                    <div className="flex items-center justify-between gap-2">
-                                        <span>Fulanito de Tal</span> <span className="text-[11px]">2/07/2024</span>
-                                    </div>
-                                    <p className="text-dark">Lorem ipsum dolor sit amet...</p>
-                                    
-                                </div>
-                            </Link>
-                    </MenuItem>
-                    <hr className="my-6 border-gray-500" />
-                    <MenuItem as='div' className='p-0 hover:bg-slate-200'>
-                            <Link to="/notifications" 
-                                className="flex flex-1 items-center gap-x-2 py-2 px-4 hover:bg-slate-200 transition-colors rounded-lg text-dark">
-                                <RiThumbUpLine className="p-2 bg-blue-100 text-blue-700 box-content rounded-full" />
-                                <div className="text-sm flex flex-col">
-                                    <div className="flex items-center justify-between gap-2">
-                                        <span>Nuevo Like</span> <span className="text-[11px]">2/07/2024</span>
-                                    </div>
-                                    <p className="text-dark">A Fulanito le gusta tu publ...</p>
-                                    
-                                </div>
-                            </Link>
-                    </MenuItem>
-                    <hr className="my-6 border-gray-500" />
-                    <MenuItem as='div' className='p-0 hover:bg-slate-200'>
-                            <Link to="/notifications" 
-                                className="flex items-center gap-x-2 py-2 px-4 hover:bg-slate-200 transition-colors rounded-lg text-dark">
-                                <RiChat3Line className="p-2 bg-yellow-100 text-yellow-500 box-content rounded-full" />
-                                <div className="text-sm flex flex-col">
-                                    <div className="flex items-center justify-between gap-2">
-                                        <span>Nuevo Comentario</span> <span className="text-[11px]">2/07/2024</span>
-                                    </div>
-                                    <p className="text-dark">Fulanito ha comentado tu ta...</p>
-                                    
-                                </div>
-                            </Link>
-                    </MenuItem>
-                    <hr className="my-6 border-gray-200" />
-                    <MenuItem as='div' className='p-0 hover:bg-slate-200 flex justify-center cursor-default'>
-                        <Link to="/" className="text-dark text-base hover:text-sky-700 transition-colors" >
-                            Todas las notificaciones
-                        </Link>
-                    </MenuItem>
-                </MenuItems>                    */}
+            <Menu>                
                 <NotificationMenu />
             </Menu>
             {/* aca comienza el menu desplegable del usuario */}
