39ff440518b4ba0b7decbbd81ef17b983e23b634
Oscar Lozano <olozano1194@gmail.com>
2026-04-09 04:29:57 -0500

    refactor(ui): implementar sistema de floating labels en componentes Input, Label y Button

--- gimnasioReact/src/components/ui/Label.tsx
+++ gimnasioReact/src/components/ui/Label.tsx
@@ -1,7 +1,7 @@
 const Label = ({children, ...props}: React.LabelHTMLAttributes<HTMLLabelElement>) => {
     return (
         
-        <label className="w-full flex flex-col justify-center items-center gap-3 font-semibold text-xl md:gap-5 lg:flex-row lg:justify-between"
+        <label className="floating-label absolute left-0 text-secondary pointer-events-none"
         {...props}
         >
             {children}
